4e9ffd2abbc6d7f5ef991e36fe9deb2202d999aa
tagine <rana@ranafayez.com>
2019-12-14 14:09:41 -0500

    local storage set

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-    $(".saveBtn").on("click, function(){
+    $(".saveBtn").on("click", function(){
         console.log($this);
-    });
+         var value = $(this).siblings(".description").val();
+         var time = $(this).parent().attr("id");
+
+         localStorage.setItem(time, value);
+});
     $("#currentday").text(moment)();
 
 });
