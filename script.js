4ce022590bf9d158c37281fbb81d2901dfb901ab
tagine <rana@ranafayez.com>
2019-12-19 16:13:54 -0500

    current date and time added as well as conditionals for color coding

--- script.js
+++ script.js
@@ -1,17 +1,70 @@
-$(document).ready(function(){
-    $(".saveBtn").on("click", function(){
-        console.log($this);
-         var value = $(this).siblings(".description").val();
-         var time = $(this).parent().attr("id");
+$(document).ready(function () {
+    $(document).on("click", ".saveBtn", function () {
+      var id = $(this).attr("id");
+      console.log(id)
+        var value = $("#text"+ id).val()
+        console.log(value)
+        var time = $(this).parent().attr("id");
+console.log(time)
+console.log(value)
+        localStorage.setItem(time, value);
+    });
+    $("#currentDay").text(moment().format("h:mma D MMM YYYY "));
 
-         localStorage.setItem(time, value);
+        // $(".space").addClass
+
+    for (var i = 9; i < 18; i++) {
+        var isBefore, isSame, isAfter;
+
+        var scheduleTime = parseInt(moment().format("HH"))
+        console.log(scheduleTime);
+
+        if (i < scheduleTime) {
+            isBefore = true
+            isSame = false
+            isAfter = false
+
+        }
+
+        else if (i === scheduleTime) {
+            isBefore = false
+            isSame = true
+            isAfter = false
+
+        }
+
+        else {
+            isBefore = false
+            isSame = false
+            isAfter = true
+
+        }
+        
+        // var iscurrentDate = moment(moment().format(i)).isAfter(moment().format("HH"), "hour");
+
+        // console.log(iscurrentDate);
+
+        $(".container").append(`
+        
+        <div class="space ${isBefore ? "past" : ""} ${isSame ? "present" : ""} ${isAfter ? "future" : ""}">
+        <div id="hour-${i}" class="row time-block"></div>
+        <div class="col-md-10 hour">${moment(i + ":00", 'HH:mm').format('h:mm a')}</div>
+        <textarea class="col-md-10 description" id="text${i}"></textarea>
+         </div>
+        <button class="btn saveBtn col-md-1" id=${i}>
+        <i class="fas fa-save"></i>
+        </button>
+
+        `)
+
+    }
+
+
+
+
+
+
 });
-    $("#currentday").text(moment)();
-
-    moment().format('MMMM Do YYYY, h:mm:ss a');
-
-});
-
 
 
 
